1b2b83c8fc0f9eb547563fa2a1f08f9aa715f06e
cazzano <cazzano.agent.47@gmail.com>
2025-01-31 14:37:04 +0500

    Important

--- stable/go/src/run.go
+++ stable/go/src/run.go
@@ -29,41 +29,83 @@ func handleRun() error {
 	// Prepare the command arguments
 	args := os.Args[2:] // Skip the "run" command
 
-	// Limit arguments to 10
-	if len(args) > 10 {
-		return fmt.Errorf("maximum of 10 arguments allowed, received %d", len(args))
-	}
+	// Check if the first argument is "-one"
+	if len(args) > 0 && args[0] == "-one" {
+		// Limit arguments to 10 (including the "-one" flag)
+		if len(args) > 11 { // 10 arguments + 1 for "-one"
+			return fmt.Errorf("maximum of 10 arguments allowed, received %d", len(args)-1)
+		}
 
-	// Construct the full command
-	cmdArgs := []string{"run"}
+		// The Go file to run is the second argument
+		if len(args) < 2 {
+			return fmt.Errorf("please provide a filename after '-one'")
+		}
 
-	// Find all .go files in the current directory
-	goFiles, err := filepath.Glob(filepath.Join(currentDir, "*.go"))
-	if err != nil {
-		return fmt.Errorf("error finding Go files: %v", err)
-	}
+		goFile := args[1]
+		if filepath.Ext(goFile) != ".go" {
+			return fmt.Errorf("the specified file must have a .go extension")
+		}
 
-	// Add Go files to the command
-	cmdArgs = append(cmdArgs, goFiles...)
+		// Construct the command
+		cmdArgs := []string{"run", goFile}
 
-	// Add user-provided arguments
-	cmdArgs = append(cmdArgs, args...)
+		// Add user-provided arguments (excluding the first two)
+		cmdArgs = append(cmdArgs, args[2:]...)
 
-	// Create the command
-	cmd := exec.Command("go", cmdArgs...)
+		// Create the command
+		cmd := exec.Command("go", cmdArgs...)
 
-	// Set the command's stdout and stderr to the current process's
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
+		// Set the command's stdout and stderr to the current process's
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Stdin = os.Stdin
 
-	// Print the command being executed (for debugging)
-	fmt.Printf("[DEBUG] Running command: go %v\n", cmdArgs)
+		// Print the command being executed (for debugging)
+		fmt.Printf("[DEBUG] Running command: go %v\n", cmdArgs)
 
-	// Run the command
-	err = cmd.Run()
-	if err != nil {
-		return fmt.Errorf("error running Go files: %v", err)
+		// Run the command
+		err = cmd.Run()
+		if err != nil {
+			return fmt.Errorf("error running Go file: %v", err)
+		}
+	} else {
+		// Original run functionality
+		// Limit arguments to 10
+		if len(args) > 10 {
+			return fmt.Errorf("maximum of 10 arguments allowed, received %d", len(args))
+		}
+
+		// Construct the full command
+		cmdArgs := []string{"run"}
+
+		// Find all .go files in the current directory
+		goFiles, err := filepath.Glob(filepath.Join(currentDir, "*.go"))
+		if err != nil {
+			return fmt.Errorf("error finding Go files: %v", err)
+		}
+
+		// Add Go files to the command
+		cmdArgs = append(cmdArgs, goFiles...)
+
+		// Add user-provided arguments
+		cmdArgs = append(cmdArgs, args...)
+
+		// Create the command
+		cmd := exec.Command("go", cmdArgs...)
+
+		// Set the command's stdout and stderr to the current process's
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Stdin = os.Stdin
+
+		// Print the command being executed (for debugging)
+		fmt.Printf("[DEBUG] Running command: go %v\n", cmdArgs)
+
+		// Run the command
+		err = cmd.Run()
+		if err != nil {
+			return fmt.Errorf("error running Go files: %v", err)
+		}
 	}
 
 	return nil
