dce301f112f1d9fc2c5c8d230faad1013a59dd73
cazzano <cazzano.kiddo@gmail.com>
2025-04-09 18:15:28 +0000

    Important

--- beta/go/src/new.go
+++ beta/go/src/new.go
@@ -1,55 +1,59 @@
 // new.go
 package main
-
 import (
-	"fmt"
-	"os"
-	"path/filepath"
+        "fmt"
+        "os"
+        "path/filepath"
 )
-
 func handleNew(folderName string) error {
-	// Create the new directory
-	err := os.MkdirAll(folderName, 0755)
-	if err != nil {
-		return fmt.Errorf("error creating directory %s: %v", folderName, err)
-	}
-	fmt.Printf("Created directory: %s\n", folderName)
-
-	// Create the src directory
-	srcDir := filepath.Join(folderName, "src")
-	err = os.MkdirAll(srcDir, 0755)
-	if err != nil {
-		return fmt.Errorf("error creating src directory: %v", err)
-	}
-	fmt.Printf("Created directory: %s\n", srcDir)
-
-	// Create the main.go file with a basic Hello World program
-	mainGoContent := `package main
-
+        // Create the new directory
+        err := os.MkdirAll(folderName, 0755)
+        if err != nil {
+                return fmt.Errorf("error creating directory %s: %v", folderName, err)
+        }
+        fmt.Printf("Created directory: %s\n", folderName)
+        
+        // Create the src directory
+        srcDir := filepath.Join(folderName, "src")
+        err = os.MkdirAll(srcDir, 0755)
+        if err != nil {
+                return fmt.Errorf("error creating src directory: %v", err)
+        }
+        fmt.Printf("Created directory: %s\n", srcDir)
+        
+        // Create the main.go file with a basic Hello World program
+        mainGoContent := `package main
 import "fmt"
-
 func main() {
-	fmt.Println("Hello, World!")
+        fmt.Println("Hello, World!")
 }
 `
-	mainGoPath := filepath.Join(srcDir, "main.go")
-	err = os.WriteFile(mainGoPath, []byte(mainGoContent), 0644)
-	if err != nil {
-		return fmt.Errorf("error creating main.go file: %v", err)
-	}
-	fmt.Printf("Created file: %s\n", mainGoPath)
-
-	// Create the go.mod file
-	goModContent := `module main
-
+        mainGoPath := filepath.Join(srcDir, "main.go")
+        err = os.WriteFile(mainGoPath, []byte(mainGoContent), 0644)
+        if err != nil {
+                return fmt.Errorf("error creating main.go file: %v", err)
+        }
+        fmt.Printf("Created file: %s\n", mainGoPath)
+        
+        // Create the go.mod file
+        goModContent := `module main
 go 1.23.5
 `
-	goModPath := filepath.Join(folderName, "go.mod")
-	err = os.WriteFile(goModPath, []byte(goModContent), 0644)
-	if err != nil {
-		return fmt.Errorf("error creating go.mod file: %v", err)
-	}
-	fmt.Printf("Created file: %s\n", goModPath)
-
-	return nil
+        goModPath := filepath.Join(folderName, "go.mod")
+        err = os.WriteFile(goModPath, []byte(goModContent), 0644)
+        if err != nil {
+                return fmt.Errorf("error creating go.mod file: %v", err)
+        }
+        fmt.Printf("Created file: %s\n", goModPath)
+        
+        // Create the .gitignore file
+        gitignoreContent := "**/target/"
+        gitignorePath := filepath.Join(folderName, ".gitignore")
+        err = os.WriteFile(gitignorePath, []byte(gitignoreContent), 0644)
+        if err != nil {
+                return fmt.Errorf("error creating .gitignore file: %v", err)
+        }
+        fmt.Printf("Created file: %s\n", gitignorePath)
+        
+        return nil
 }
